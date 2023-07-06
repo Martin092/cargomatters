d0ecf7195fbaff8f28965ca6eb259412d255d621
Martin Damyanov <m.damyanov@student.tudelft.nl>
2023-07-06 13:41:50 +0200

    feat added link to the benefit component

--- src/lib/Benefit.svelte
+++ src/lib/Benefit.svelte
@@ -11,7 +11,7 @@
         <h6 class="h6">International Partners</h6>
         <h3 class="h3">Solutions to all your problems</h3>
         <p>{benefit.message}</p>
-        <p class="font-bold underline underline-offset-4">{benefit.link}</p>
+        <a href={benefit.link} class="font-bold underline underline-offset-4">{benefit.linkMessage}</a>
     </div>
 <img src={benefit.photo} alt={benefit.alt} class="md:w-1/2 rounded-2xl">
 
--- src/lib/typescript/benefit.ts
+++ src/lib/typescript/benefit.ts
@@ -2,12 +2,14 @@ export class BenefitClass {
     message: string;
     photo: string;
     alt: string;
+    linkMessage: string;
     link: string;
 
-    constructor(message:string, photo:string, alt:string, link:string){
+    constructor(message:string, photo:string, alt:string, linkMessage: string, link:string){
         this.message = message;
         this.photo = photo;
         this.alt = alt;
         this.link = link;
+        this.linkMessage = linkMessage;
     }
 }
