79350f57d54feacb37f0a1fd656300470ed85fc9
Zakrok09 <eccyboo@protonmail.com>
2023-07-07 23:15:14 +0300

    feat: benefits in about page

--- src/lib/components/home/Benefits.svelte
+++ src/lib/components/home/Benefits.svelte
@@ -1,5 +1,5 @@
 <script>
-    import {Benefit} from "$lib/components/reusable/index";
+    import {Benefit} from "$lib/components/reusable";
     import {BenefitClass} from "$lib/typescript/benefit";
 
     let benefit1 = new BenefitClass("International Partners", "Solutions to all problems", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue.", "images/gray.png", "alt", "Explore services >", "/services");
--- src/lib/components/reusable/Benefit.svelte
+++ src/lib/components/reusable/Benefit.svelte
@@ -4,12 +4,16 @@
     export let benefit:BenefitClass;
 </script>
 
-<div class="flex flex-col-reverse md:flex-row xl:flex-row text-secondary-500
-            gap-4 md:gap-4 xl:gap-6">
-    <div class="flex flex-col gap-2 md:gap-4 xl:gap-4 md:w-1/2 xl:w-1/2">
-        <h6 class="mb-1 text-secondary-400">{benefit.subTitle}</h6>
-        <h3 class="mb-2 font-semibold">{benefit.mainTitle}</h3>
-        <p class="text-secondary-400">{benefit.message}</p>
+<div class="flex flex-col-reverse md:flex-row text-secondary-500
+            gap-4 xl:gap-6">
+    <div class="flex flex-col gap-8 md:w-1/2">
+        <div class="gap-4">
+            <div class="gap-2">
+                <h6 class="mb-1 text-secondary-400">{benefit.subTitle}</h6>
+                <h3 class="mb-2 font-semibold">{benefit.mainTitle}</h3>
+            </div>
+            <p class="text-secondary-400">{benefit.message}</p>
+        </div>
         <a href={benefit.link} class="font-bold xl:font-extrabold underline underline-offset-4 max-md:text-[12px]">
             {benefit.linkMessage}
         </a>
