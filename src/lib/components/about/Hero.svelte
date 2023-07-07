def5235b0b1a537329bd6a6285d9233a50a828dc
Zakrok09 <eccyboo@protonmail.com>
2023-07-08 02:00:24 +0300

    fix: bug on about page breaking on mobile

--- src/lib/components/about/Hero.svelte
+++ src/lib/components/about/Hero.svelte
@@ -1,7 +1,7 @@
 <div id="hero"  class="col-start-1 row-start-1 col-span-full"></div>
 
-<div class="col-span-4 flex flex-col row-start-1 pt-80 pb-20
-            md:col-span-6 md:gap-12
+<div class="col-start-1 row-start-1 col-span-4 flex flex-col pt-80 pb-20
+            md:col-start-1 md:col-span-6 md:gap-12
             xl:col-start-2 xl:col-span-12">
     <div class="flex flex-col gap-4">
         <h1>About Us</h1>
