97672ab7ea3dc470e299d5fae7e96548eefb07f7
Zakrok09 <eccyboo@protonmail.com>
2023-07-08 00:51:06 +0300

    fix: about margin and links in footer

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,7 +9,7 @@
 <main class="text-gray-800
              grid grid-cols-4 gap-x-2 mx-4
              md:grid-cols-8 md:gap-x-4 md:mx-8
-             xl:grid-cols-12 xl:gap-x-6 xl:mx-32 xl:mt-[-80px]">
+             xl:grid-cols-12 xl:gap-x-6 xl:mx-32">
 
     <!--  Hero section  -->
     <Hero />
