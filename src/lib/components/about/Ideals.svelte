f8cecd5566491394362fdc0bd5750b37b828001d
Zakrok09 <eccyboo@protonmail.com>
2023-07-07 23:25:52 +0300

    feat: ideals in about page

---
+++ src/lib/components/about/Ideals.svelte
@@ -0,0 +1,27 @@
+
+<div id="ideals" class="col-start-1 col-span-full row-start-3 w-screen
+                              ml-[-16px]
+                              md:ml-[-32px]
+                              xl:ml-[-128px]">s</div>
+<div class="row-start-3 col-start-1 col-span-full py-20 text-gray-100 text-center">
+    <div class="flex flex-col gap-10
+                xl:gap-20">
+        <h3>We believe in:</h3>
+        <div class="flex flex-row justify-evenly gap-4 flex-wrap md:flex-nowrap">
+            <h4>Efficiency</h4>
+            <h4>Honesty</h4>
+            <h4>Ambition</h4>
+            <h4>Kindness</h4>
+            <h4>Security</h4>
+            <h4>Safety</h4>
+        </div>
+    </div>
+</div>
+
+<style lang="scss">
+  #ideals {
+    background-image: url("images/feature-background.png");
+    background-position: top right;
+    background-size: cover;
+  }
+</style>
