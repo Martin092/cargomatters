2e04cc70409cf9d1492e263dfad4b78f1c780f6a
Zakrok09 <eccyboo@protonmail.com>
2023-07-07 17:41:13 +0300

    fix: design of review and benefit components

--- src/lib/Benefit.svelte
+++ src/lib/Benefit.svelte
@@ -4,15 +4,16 @@
     export let benefit:BenefitClass;
 </script>
 
-<div class="flex flex-col-reverse md:flex-row xl:flex-row
+<div class="flex flex-col-reverse md:flex-row xl:flex-row text-secondary-500
             gap-4 md:gap-4 xl:gap-6">
     <div class="flex flex-col gap-2 md:gap-4 xl:gap-6 md:w-1/2 xl:w-1/2"> 
-        <h6 class="max-md:text-xs mb-1
+        <h6 class="max-md:text-xs mb-1 text-secondary-400
                    max-xl:text-[13px]">{benefit.subTitle}</h6>
-        <h3 class="max-md:text-2xl mb-2">{benefit.mainTitle}</h3>
-        <p class="max-md:text-xs">{benefit.message}</p>
-        <a href={benefit.link} class="font-bold xl:font-extrabold underline underline-offset-4 max-md:text-[12px]
-                                      ">{benefit.linkMessage}</a>
+        <h3 class="max-md:text-2xl mb-2 font-semibold">{benefit.mainTitle}</h3>
+        <p class="max-md:text-xs text-secondary-400">{benefit.message}</p>
+        <a href={benefit.link} class="font-bold xl:font-extrabold underline underline-offset-4 max-md:text-[12px]">
+            {benefit.linkMessage}
+        </a>
     </div>
 <img src={benefit.photo} alt={benefit.alt} class="md:w-1/2 rounded-lg">
 
