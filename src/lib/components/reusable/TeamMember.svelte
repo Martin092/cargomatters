93408585c94e00133fb92331718feb33cc004114
Zakrok09 <eccyboo@protonmail.com>
2023-07-07 23:39:36 +0300

    feat: members in about page

--- src/lib/components/reusable/TeamMember.svelte
+++ src/lib/components/reusable/TeamMember.svelte
@@ -4,9 +4,7 @@
 </script>
 
 <div class="rounded-lg">
-    <div>
-        <img src={member.image} alt={member.alt}>
-    </div>
+    <img src={member.image} alt={member.alt}>
     <div class="flex flex-col bg-[#313131] gap-2 md:gap-4 xl:gap-6 p-5">
         <h6 class="text-gray-100">{member.position}</h6>
         <h4 class="text-gray-100 mb-5">{member.name}</h4>
