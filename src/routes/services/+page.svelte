74d13cc3a8347f168b123d389b3b182a52bb5eb7
Zakrok09 <eccyboo@protonmail.com>
2023-07-09 00:07:32 +0300

    style: clean up unused imports

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,15 +1,4 @@
 <script lang="ts">
-    import {EmergencyServiceClass, Stat} from "$lib/typescript";
-
-    let stat1 = new Stat(12, "flights");
-    let stat2 = new Stat(13, "trucks");
-    let stat3 = new Stat(10, "planes");
-
-    let stats:Stat[] = [stat1, stat2, stat3];
-
-    let testEmergency =
-        new EmergencyServiceClass("/", "alt", "/", "fast", "title", "none", stats);
-
     import {Hero, WorkProcess} from "$lib/components/services/index"
     import {FAQ, CTA} from "$lib/components/home/index";
     import Header from "$lib/components/Header.svelte";
