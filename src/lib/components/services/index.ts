cc31c52ae4abddeb3596b7b0b1129993a3658d43
Zakrok09 <eccyboo@protonmail.com>
2023-07-09 02:13:18 +0300

    feat: responsive emergency solutions catalogue

--- src/lib/components/services/index.ts
+++ src/lib/components/services/index.ts
@@ -2,5 +2,7 @@ import Hero from './Hero.svelte';
 import WorkProcess from "./WorkProcess.svelte";
 import SubHero from "./SubHero.svelte";
 import TransportSolutions from "./TransportSolutions.svelte";
+import TransportComponent from "./TransportComponent.svelte";
+import EmergencySolutions from "./EmergencySolutions.svelte";
 
-export {Hero, WorkProcess, SubHero, TransportSolutions};
+export {Hero, WorkProcess, SubHero, TransportSolutions, TransportComponent, EmergencySolutions};
