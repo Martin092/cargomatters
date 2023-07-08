1b1284870bceed934ec0610e19692404c98db072
Zakrok09 <eccyboo@protonmail.com>
2023-07-09 01:43:26 +0300

    fix: main solution component

--- src/lib/components/services/Hero.svelte
+++ src/lib/components/services/Hero.svelte
@@ -2,9 +2,13 @@
     import {MainSolutions} from "$lib/components/reusable";
     import {MainSolution} from "$lib/typescript";
 
-    let mainSolution = new MainSolution("/images/gray.png", "/images/gray.png", "Industrial", "description",
-                                        ["Road Freight", "Sea Freight", "Air Freight", "Rail Freight", "Emergency Transport Services"],
-                                        "/services/transport")
+    let transportSolution = new MainSolution("/images/gray.png", "/images/gray.png", "Transport", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.",
+        ["Road Freight", "Sea Freight", "Air Freight", "Rail Freight", "Emergency Transport"],
+        "/services/transport")
+
+    let industrySolution = new MainSolution("/images/gray.png", "/images/gray.png", "Logistics & Industry", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.",
+        ["Business Solutions", "Customs handling", "Integrated services", "Distributions", "Cargo Insurance", "Claim Handling", "Added Value Services"],
+        "/services/industry")
 </script>
 
 
@@ -20,10 +24,10 @@
 <div class="row-start-2 col-start-1 col-span-full flex flex-col gap-2 py-40 pb-20 mt-[-420px]
             md:flex-row
             lg:col-start-2 lg:col-span-6
-            xl:col-start-3 xl:col-span-8
-            2xl:col-start-4 2xl:col-span-6">
-    <MainSolutions {mainSolution} />
-    <MainSolutions {mainSolution} />
+            xl:col-start-2 xl:col-span-10
+            2xl:col-start-3 2xl:col-span-8">
+    <MainSolutions mainSolution={transportSolution} />
+    <MainSolutions mainSolution={industrySolution} />
 </div>
 
 <style lang="scss">
