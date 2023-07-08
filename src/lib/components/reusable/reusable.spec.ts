f689b00f0c2d44be3ec981c1bd46aba94fa5b03d
Zakrok09 <eccyboo@protonmail.com>
2023-07-08 11:56:58 +0300

    test: delete useless unit test

--- src/lib/components/reusable/reusable.spec.ts
+++ src/lib/components/reusable/reusable.spec.ts
@@ -1,28 +1,19 @@
-import { describe, it, expect } from 'vitest';
-import {render, screen} from '@testing-library/svelte';
+import { describe, it, expect, afterEach } from 'vitest';
+import {cleanup, render, screen} from '@testing-library/svelte';
 
 import Feature from "./Feature.svelte";
 import {FeatureClass} from "../../typescript/feature";
 
+
+afterEach(() => {
+    cleanup()
+})
+
 describe('Feature component', () => {
-    it('renders', () => {
-
-        let testFeature = new FeatureClass("-","Feature", "desc", "/", "Home");
-
-        render(Feature, {feature: testFeature});
-
-        const heading = screen.getByText("Feature");
-
-        expect(heading).toBeTruthy();
-
-    });
-
     it('renders correct link', async () => {
-
         let testFeature = new FeatureClass("-","Feature", "desc", "/", "Home");
 
         render(Feature, {feature: testFeature});
-
         const link = screen.getByText("Home");
 
         expect(link).toBeTruthy();
