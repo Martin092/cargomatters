35c35a9778a7d31c4a50799f0e1a1c246f7fdf49
Zakrok09 <eccyboo@protonmail.com>
2023-07-10 23:17:42 +0300

    docs: SvelteDoc corrections and move prop doc to the prop declarations

--- src/lib/typescript/BussinessPageLoader.ts
+++ src/lib/typescript/BussinessPageLoader.ts
@@ -3,18 +3,39 @@ import type {IndustrySolutionClass} from "./industrySolutions";
 import type {BusinessDao} from "./businessDao";
 import type {PagesDatabase} from "./PagesDatabase";
 
+/**
+ * Implementation (port) for the BusinessDao adapter. Utilizes a page database
+ * of class PageDatabase which uses a private hash map with all pages
+ */
 export class BusinessPageLoader implements BusinessDao {
     private pagesDatabase:PagesDatabase;
 
+    /**
+     * Generate BusinessPageLoader - a port for the BusinessDao adapter
+     * @param pagesDatabase - the 'database' with the pages. Allows for
+     * dependency injection for mocking.
+     */
     constructor(pagesDatabase:PagesDatabase) {
         this.pagesDatabase = pagesDatabase;
     }
 
+    /**
+     * Get the page information as a BusinessPage class or null
+     * by the type keyword of the business.
+     * @param type the type keyword of the business e.g. aerospace, military etc.
+     * @return the business page information as a BusinessPage class or null if that
+     * business is not found in the database
+     */
     public getPage(type:string):BusinessPage|null {
         let res = this.pagesDatabase.getPage(type);
         return (res) ? res: null;
     }
 
+    /**
+     * Get the catalogue of all industrial solutions to load them on
+     * the logistic & business solutions page.
+     * @return a catalogue with all business solutions in the database
+     */
     public getCatalogue():IndustrySolutionClass[] {
         let res:IndustrySolutionClass[] = [];
 
--- src/lib/typescript/PagesDatabase.ts
+++ src/lib/typescript/PagesDatabase.ts
@@ -1,14 +1,27 @@
 import type {BusinessPage} from "./businessPage";
 
+/**
+ * Singleton PageDatabase class to store the business pages. Used for the naive
+ * server implementation of loading the business page information dynamically
+ * using only the keyword of the business. Could be disregarded if switched for
+ * a headless CMS.
+ */
 export class PagesDatabase {
     private static instance: PagesDatabase;
-
     private _pages:Map<string, BusinessPage>;
 
+    /**
+     * A private instructor to create a PagesDatabase if one is
+     * not yet created. Singleton approach is used to ensure
+     * that a single 'database' class is used.
+     */
     private constructor() {
         this._pages = new Map<string, BusinessPage>();
     }
 
+    /**
+     * Get instance of the PagesDatabase singleton.
+     */
     public static getInstance():PagesDatabase {
         if (!PagesDatabase.instance) {
             PagesDatabase.instance = new PagesDatabase();
@@ -17,14 +30,26 @@ export class PagesDatabase {
         return PagesDatabase.instance;
     }
 
+    /**
+     * Add a page to the database by providing a BusinessPage class
+     * @param page the page to be added as a BusinessPage class
+     */
     public addPage(page:BusinessPage):void {
         this._pages.set(page.type.toLowerCase(), page);
     }
 
+    /**
+     * Get a page from the database
+     * @param type the type keyword of the page e.g. aerospace, military etc.
+     */
     public getPage(type:string):BusinessPage|undefined {
-        return this._pages.get(type);
+        return this._pages.get(type.toLowerCase());
     }
 
+    /**
+     * A getter method for a reference to the hashmap database implementation
+     * @return the database hashmap
+     */
     get pages(): Map<string, BusinessPage> {
         return this._pages;
     }
